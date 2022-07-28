6ff30714be734fe774929aaee2af60d2e5cbf6b4
Raghu K <raghuk.vit@gmail.com>
2022-07-28 06:39:48 +0530

    Update terraform/roboshop-terraform-mutable/

--- env/dev.tfvars
+++ env/dev.tfvars
@@ -1,3 +1,5 @@
-ENV      = "dev"
-PROJECT  = "roboshop"
-VPC_CIDR = "10.50.0.0/16"
+ENV                  = "dev"
+PROJECT              = "roboshop"
+VPC_CIDR             = "10.50.0.0/16"
+PUBLIC_SUBNETS_CIDR  = ["10.50.0.0/24", "10.50.1.0/24"]
+PRIVATE_SUBNETS_CIDR = ["10.50.2.0/24", "10.50.3.0/24"]
--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "VPC" {
-  source   = "github.com/raghudevopsb65/tf-module-vpc"
-  ENV      = var.ENV
-  PROJECT  = var.PROJECT
-  VPC_CIDR = var.VPC_CIDR
+  source               = "github.com/raghudevopsb65/tf-module-vpc"
+  ENV                  = var.ENV
+  PROJECT              = var.PROJECT
+  VPC_CIDR             = var.VPC_CIDR
+  PUBLIC_SUBNETS_CIDR  = var.PUBLIC_SUBNETS_CIDR
+  PRIVATE_SUBNETS_CIDR = var.PRIVATE_SUBNETS_CIDR
 }
+
