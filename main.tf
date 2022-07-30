1fb0ad9e819acf5caefcc23445c4db7161455a4b
Raghu K <raghuk.vit@gmail.com>
2022-07-30 06:23:13 +0530

    Update terraform/roboshop-terraform-mutable/

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ module "VPC" {
   DEFAULT_VPC_RT       = var.DEFAULT_VPC_RT
 }
 
-//module "RDS" {
-//  source             = "github.com/raghudevopsb65/tf-module-rds"
-//  ENV                = var.ENV
-//  PROJECT            = var.PROJECT
-//  ENGINE             = var.ENGINE
-//  ENGINE_VERSION     = var.ENGINE_VERSION
-//  RDS_INSTANCE_CLASS = var.RDS_INSTANCE_CLASS
-//  PG_FAMILY          = var.PG_FAMILY
-//  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
-//  VPC_ID             = module.VPC.VPC_ID
-//  RDS_PORT           = var.RDS_PORT
-//  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_IDS
-//}
+module "RDS" {
+  source             = "github.com/raghudevopsb65/tf-module-rds"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  ENGINE             = var.ENGINE
+  ENGINE_VERSION     = var.ENGINE_VERSION
+  RDS_INSTANCE_CLASS = var.RDS_INSTANCE_CLASS
+  PG_FAMILY          = var.PG_FAMILY
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  RDS_PORT           = var.RDS_PORT
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_IDS
+}
 
