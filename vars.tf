99a0cd3cb883af8ab28739ddfbd76727bbd08632
Raghu K <raghuk.vit@gmail.com>
2022-07-30 06:20:08 +0530

    Update terraform/roboshop-terraform-mutable/

--- vars.tf
+++ vars.tf
@@ -16,7 +16,5 @@ variable "ENGINE" {}
 variable "ENGINE_VERSION" {}
 variable "RDS_INSTANCE_CLASS" {}
 variable "PG_FAMILY" {}
-variable "PRIVATE_SUBNET_IDS" {}
-variable "VPC_ID" {}
 variable "RDS_PORT" {}
-variable "ALLOW_SG_CIDR" {}
+
