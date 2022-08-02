47507792b079fda690dde70af9ebb14a551c64d9
Raghu K <raghuk.vit@gmail.com>
2022-08-02 06:55:52 +0530

    Update terraform/roboshop-terraform-mutable/

--- main.tf
+++ main.tf
@@ -92,5 +92,78 @@ module "FRONTEND" {
   LB_ARN             = module.LB.PUBLIC_LB_ARN
 }
 
+module "CART" {
+  source             = "github.com/raghudevopsb65/tf-module-mutable-app"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_CIDR
+  PORT               = 8080
+  COMPONENT          = "cart"
+  INSTANCE_TYPE      = "t3.micro"
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  INSTANCE_COUNT     = var.INSTANCE_COUNT["CART"]["COUNT"]
+  LB_ARN             = module.LB.PRIVATE_LB_ARN
+}
 
+module "CATALOGUE" {
+  source             = "github.com/raghudevopsb65/tf-module-mutable-app"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_CIDR
+  PORT               = 8080
+  COMPONENT          = "catalogue"
+  INSTANCE_TYPE      = "t3.micro"
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  INSTANCE_COUNT     = var.INSTANCE_COUNT["CATALOGUE"]["COUNT"]
+  LB_ARN             = module.LB.PRIVATE_LB_ARN
+}
+
+module "USER" {
+  source             = "github.com/raghudevopsb65/tf-module-mutable-app"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_CIDR
+  PORT               = 8080
+  COMPONENT          = "user"
+  INSTANCE_TYPE      = "t3.micro"
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  INSTANCE_COUNT     = var.INSTANCE_COUNT["USER"]["COUNT"]
+  LB_ARN             = module.LB.PRIVATE_LB_ARN
+}
+
+module "SHIPPING" {
+  source             = "github.com/raghudevopsb65/tf-module-mutable-app"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_CIDR
+  PORT               = 8080
+  COMPONENT          = "shipping"
+  INSTANCE_TYPE      = "t3.micro"
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  INSTANCE_COUNT     = var.INSTANCE_COUNT["SHIPPING"]["COUNT"]
+  LB_ARN             = module.LB.PRIVATE_LB_ARN
+}
+
+module "PAYMENT" {
+  source             = "github.com/raghudevopsb65/tf-module-mutable-app"
+  ENV                = var.ENV
+  PROJECT            = var.PROJECT
+  PRIVATE_SUBNET_IDS = module.VPC.PRIVATE_SUBNET_IDS
+  VPC_ID             = module.VPC.VPC_ID
+  ALLOW_SG_CIDR      = module.VPC.PRIVATE_SUBNET_CIDR
+  PORT               = 8080
+  COMPONENT          = "payment"
+  INSTANCE_TYPE      = "t3.micro"
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  INSTANCE_COUNT     = var.INSTANCE_COUNT["PAYMENT"]["COUNT"]
+  LB_ARN             = module.LB.PRIVATE_LB_ARN
+}
 
